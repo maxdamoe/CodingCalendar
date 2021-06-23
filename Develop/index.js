93a1c5837be89a32314e6b9b916279d969d8e5bf
msimp <max.simpson@gmail.com>
2021-06-23 12:46:34 -0500

    first commit, decent HTML and CSS

---
+++ Develop/index.js
@@ -0,0 +1,61 @@
+// function to display current day / time at top of page
+let Today = document.getElementById("currentDay")
+Today.innerHTML = moment()
+
+// getting minutes of the hour
+
+let M = moment()
+let timeAddition = M.subtract()
+let getHour = M.get("hour")
+
+
+console.log(getHour)
+
+//if (getHour !==  
+
+//IF the schedule hour is in the past based on the current hour, THEN make bar grey
+//IF the schedule hour is the current hour, THEN make bar red
+//IF the schedule hour is in the future based on the current hour, THEN make the bar green
+
+
+
+
+
+
+
+
+
+//if it is an old section, behind the current time, we need the bar to be grey
+
+
+
+//if happening is before current Time, then make the box grey
+
+
+//function inPast (){
+
+
+    // this function will check to see if it is in the past, and if it is, assign to the html the class of past, applying the css for the class past.
+    // YOU CAN USE MULTIPLE CLASSES ON THE SAME ELEMENT!! YES!!!
+
+// if it is the current hour, then we need the bar to be red
+
+// if the bar is in the future, then we need the bar to be green.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if save button clicked, we need to add to local storage
+
+//let happening = document.getElementsByClassName("clicked")
